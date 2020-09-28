2442409afb16830dbb6fa6cb5e264ce9c4eaa785
Collins Charles <collinspro18@gmail.com>
2020-09-28 16:26:07 +0100

    Functional base component with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import About from "./components/pages/About";
@@ -9,183 +9,83 @@ import Users from './components/users/Users';
 import User from "./components/users/User";
 import './App.css';
 
-class App extends Component {
-    state = {
-        // users: [
-        //     {
-        //         id: '1',
-        //         login: 'mojonbo',
-        //         avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/mojombo'
-        //     },
-        //     {
-        //         id: '2',
-        //         login: 'collins',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/collins'
-        //     },
-        //     {
-        //         id: '3',
-        //         login: 'kelvin',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/collins'
-        //     },
-        //     {
-        //         id: '4',
-        //         login: 'wilson',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/collins'
-        //     },
-        //     {
-        //         id: '5',
-        //         login: 'charles',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/collins'
-        //     },
-        //     {
-        //         id: '6',
-        //         login: 'joshua',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/collins'
-        //     },
-        //     {
-        //         id: '7',
-        //         login: 'peace',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/kelvin'
-        //     },
-        //     {
-        //         id: '8',
-        //         login: 'hope',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/kelvin'
-        //     },
-        //     {
-        //         id: '9',
-        //         login: 'blessing',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/kelvin'
-        //     },
-        //     {
-        //         id: '10',
-        //         login: 'miracle',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/kelvin'
-        //     },
-        //     {
-        //         id: '11',
-        //         login: 'faith',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/kelvin'
-        //     },
-        //     {
-        //         id: '12',
-        //         login: 'anegbe',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/kelvin'
-        //     },
-        //     {
-        //         id: '13',
-        //         login: 'abadaike',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/kelvin'
-        //     },
-        //     {
-        //         id: '14',
-        //         login: 'igwe',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/kelvin'
-        //     },
-        //     {
-        //         id: '15',
-        //         login: 'confidence',
-        //         avatar_url: 'https://avatars3.githubusercontent.com/u/1?v=4',
-        //         html_url: 'https://github/kelvin'
-        //     },
-        // ],
-         user: {},
-        users: [],
-        repos: [],
-      loading: false,
-        alert: null
-    };
-
-    // Get Fired once Components loads
-    // Get all Users from API
-    async componentDidMount() {
-      this.setState({ loading: true });
-      const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-      this.setState({  users: res.data, loading: false });
-    }
+const App = () => {
+    const [ user, setUser ] = useState({});
+    const [ users, setUsers ] = useState([]);
+    const [ repos, setRepos ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
+    const [ alert, setAlert ] = useState(null);
 
     // Search GitHub Users
-    searchUsers = async (name) => {
-        this.setState({ loading: true });
+    const searchUsers = async (name) => {
+        setLoading(true);
         const res = await axios.get(`https://api.github.com/search/users?q=${name}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-        this.setState({  users: res.data.items, loading: false });
+        setUsers(res.data.items)
+        setLoading(false);
     };
 
     // Get Single User
     // @param: Login as Username
-    getUserRepos = async (username) => {
-        this.setState({ loading: true });
+    const getUserRepos = async (username) => {
+        setLoading(true);
         const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-        this.setState({  user: res.data, loading: false });
+        setUser(res.data);
+        setLoading(false);
     };
 
     // Get User Repos
-    getUser = async (username) => {
-        this.setState({ loading: true });
+    const getUser = async (username) => {
+        setLoading(true);
         const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-        this.setState({  repos: res.data, loading: false });
+        setRepos(res.data);
+        setLoading(false);
     };
 
     //  Clear Users from State
-    clearUsers = () => this.setState({users: [], loading: false});
+    const clearUsers = () => {
+        setUsers([]);
+        setLoading(false);
+    }
 
     // Show Alert
-    setAlert = (msg, type) => {
-        this.setState({ alert: {msg, type} });
-        setTimeout(() => this.setState({alert: null}), 3000);
+    const showAlert = (msg, type) => {
+        setAlert({msg, type});
+        setTimeout(() => setAlert(null), 3000);
     };
-
-    render() {
-        const { user, users, loading, alert, repos } = this.state;
-       return (
-           <Router>
-              <div className="App">
-                 <Navbar />
-                 <div className={'container'}>
-                     <Alert alert={alert} />
-                     <Switch>
-                         <Route exert path={'/about'} component={About}/>
-                         <Route exert path={'/user/:login'} render={props => (
-                             <User 
-                                {...props} 
-                                getUser={this.getUser} 
-                                getUserRepos={this.getUserRepos} 
-                                user={user} 
-                                repos={repos} 
-                                loading={loading} 
-                            />
-                         )}/>
-                         <Route exert path={'/'} render={props => (
-                             <Fragment>
-                                 <Search
-                                     searchUsers={this.searchUsers}
-                                     clearUsers={this.clearUsers}
-                                     showClear={users.length > 0 }
-                                     setAlert={this.setAlert}
-                                 />
-                                 <Users users={users} loading={loading} />
-                             </Fragment>
-                         )}/>
-                     </Switch>
-                 </div>
-              </div>
-           </Router>
-       )
-    }
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className={'container'}>
+                    <Alert alert={alert} />
+                    <Switch>
+                        <Route exert path={'/about'} component={About}/>
+                        <Route exert path={'/user/:login'} render={props => (
+                            <User 
+                            {...props} 
+                            getUser={getUser} 
+                            getUserRepos={getUserRepos} 
+                            user={user} 
+                            repos={repos} 
+                            loading={loading} 
+                        />
+                        )}/>
+                        <Route exert path={'/'} render={props => (
+                            <Fragment>
+                                <Search
+                                    searchUsers={searchUsers}
+                                    clearUsers={clearUsers}
+                                    showClear={users.length > 0 }
+                                    setAlert={showAlert}
+                                />
+                                <Users users={users} loading={loading} />
+                            </Fragment>
+                        )}/>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    )
 }
 
 export default App;
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,42 +1,37 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 import PropTypes from 'prop-types';
 
-class Search extends Component {
-    state = {
-        name: ''
-    };
+const Search = ({showClear, clearUsers, searchUsers, setAlert}) => {
+    const [name, setName] = useState('');
 
-    static propTypes  = {
-        searchUsers: PropTypes.func.isRequired,
-         clearUsers: PropTypes.func.isRequired,
-          showClear: PropTypes.bool.isRequired,
-           setAlert: PropTypes.func.isRequired
-    };
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        if (this.state.name === '') {
-            this.props.setAlert('Please enter something!!', 'light');
+        if (name === '') {
+            setAlert('Please enter something!!', 'light');
         } else {
-            this.props.searchUsers(this.state.name);
-            this.setState({name: ''});
+            searchUsers(name);
+            setName('');
         }
     };
-    onChange = (e) => this.setState({[e.target.name]: e.target.value});
+    const onChange = (e) => setName(e.target.value);
 
-    render() {
-        const { showClear, clearUsers } = this.props;
-        return (
-            <div>
-                <form onSubmit={this.onSubmit} className={'form'}>
-                    <input type='text' name={'name'} placeholder={'Search Users...'} value={this.state.name} onChange={this.onChange} />
-                    <input type="submit" value="Search" className={'btn btn-dark btn-block'} />
-                </form>
-                {showClear && (
-                <button className={'btn btn-light btn-block'} onClick={clearUsers}>Clear</button>
-                )}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <form onSubmit={onSubmit} className={'form'}>
+                <input type='text' name={'name'} placeholder={'Search Users...'} value={name} onChange={onChange} />
+                <input type="submit" value="Search" className={'btn btn-dark btn-block'} />
+            </form>
+            {showClear && (
+            <button className={'btn btn-light btn-block'} onClick={clearUsers}>Clear</button>
+            )}
+        </div>
+    )
 }
+
+Search.propTypes  = {
+    searchUsers: PropTypes.func.isRequired,
+     clearUsers: PropTypes.func.isRequired,
+      showClear: PropTypes.bool.isRequired,
+       setAlert: PropTypes.func.isRequired
+};
 export default Search;
