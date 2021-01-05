be3c4644c48a62425a24e71f9e6903feb2ee3050
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-05 02:21:53 +0100

    updated alert display

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -5,12 +5,12 @@ const Alert = () => {
 	const alertContext = useContext(AlertContext);
 	const { alert } = alertContext;
 
-	return (
-		alert !== null && (
-			<div className={`alert alert-${alert.type}`}>
-				<i className={'fas fa-info-circle'} /> {alert.msg}
-			</div>
-		)
+	return alert && alert.msg ? (
+		<div className={`alert alert-${alert.type}`}>
+			<i className={'fas fa-info-circle'} /> {alert.msg}
+		</div>
+	) : (
+		<div></div>
 	);
 };
 export default Alert;
